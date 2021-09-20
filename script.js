d5ddb49ed33cbd7900acef60a30f373de76aaa45
Jacob <yawjacob@yahoo.com>
2021-09-19 19:43:33 -0500

    added button

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content add by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  function topFunction() {
+    document.documentElement.scrollTop = 0;
+  }
